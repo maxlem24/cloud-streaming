df120128a4272f96145c2a2ec86393918622d3ca
Saitilink <menu.thomas1@gmail.com>
2025-10-06 15:38:51 +0200

    ALED

--- edge-cluster/init_edge_cluster.py
+++ edge-cluster/init_edge_cluster.py
@@ -1,12 +1,13 @@
+import json
 import random
 import time
 from paho.mqtt import client as mqtt_client
-import json
 
 # MQTT Configuration
-BROKER = '10.207.130.234'
+BROKER = 'localhost'
 PORT = 1883
-TOPIC = "/python/mqtt"
+TOPIC = "auth/user/"
+TOPIC_ID = "auth/user/ID/"
 CLIENT_ID = f'python-mqtt-{random.randint(0, 1000)}'
 
 
@@ -28,46 +29,58 @@ def connect_mqtt():
 def publish(client, topic, message):
     """Publish messages to MQTT topic"""
     msg_count = 0
-    while True:
-        time.sleep(1)
-        msg = f"messages: {msg_count}"
-        result = client.publish(topic, msg)
+    result = client.publish(topic, message)
+    # Check publish status
+    status = result[0]
+    if status == 0:
+        print(f"message lancé vers {topic}")
+    else:
+        print(f"Failed to send message to topic {TOPIC}")
+    msg_count += 1
         
-        # Check publish status
-        status = result[0]
-        if status == 0:
-            print(f"Send `{msg}` to topic `{TOPIC}`")
-        else:
-            print(f"Failed to send message to topic {TOPIC}")
-        msg_count += 1
-        
-        
+
 def subscribe(client: mqtt_client, topic: str):
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-        if (msg.topic=="auth/user"):
+        if (msg.topic=="auth/user/"):
             message_json=json.loads(msg.payload.decode())
             user=message_json["user"]
             password=message_json["password"]
             ID=message_json["ID"]
-        #VERIF niveau bdd le user
-        msg_a_envoyer={"param1":"param... A REMPLIR######################","status":"ok"}
-        message_json=json.dumps(msg_a_envoyer)
-        publish(client,f"auth/user/{ID}", message_json)
 
+        if (msg.topic==f"auth/user/{CLIENT_ID}/"):
+            print("Message reçu depuis auth/user/ID/")
 
+        if (msg.topic=="db/"):
+
+            ID = "A remplacer quand on aura le format des paquets"
+            
+            if 'db is present':
+                # send db 
+                print("TODO: send DB")
+                publish(client,f"db/{ID}")
+
+            else:
+                '''db non presente'''
+                payload = json.dumps({'DB' : "Empty"})
+                publish(client,f"db/{ID}",payload)
+        if (msg.tpoic == f"db/{CLIENT_ID}"):
+            ''
     client.subscribe(topic)
     client.on_message = on_message
 
+def premiere_connexion(client):
+    # On envoie notre ID au serveur.
+    publish(client,"auth/user/",CLIENT_ID)
+    publish(client,"db/",CLIENT_ID)
+
 
 def run():
-    # on reçoi : user, password, ID
-    
+    """Main function to run MQTT client"""
     client = connect_mqtt()
+    subscribe(client, TOPIC)
+    subscribe(client,TOPIC_ID)      
     client.loop_forever()
-    
-    subscribe(client, f"auth/user/")
-    
 
 
 if __name__ == '__main__':
