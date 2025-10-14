5f80ef60ecadcac4537c7074764c2b5815d878b2
DYSTOpyy <bcd.saltel@gmail.com>
2025-10-14 09:45:55 +0200

    add edge authentification

--- edge-cluster/pour_authentification_edge.py
+++ edge-cluster/pour_authentification_edge.py
@@ -1,7 +1,32 @@
-    subscribe(client, "auth/zone")  
-    subscribe(client, "auth/user")  
+   
+def connect_mqtt():
+    """Connect to MQTT broker"""
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
 
+    client = mqtt_client.Client()
+    client.on_connect = on_connect
+    client.connect(BROKER, PORT)
+    return client
 
+def publish(client, topic, message):
+    """Publish messages to MQTT topic"""
+    result = client.publish(topic, message)
+    status = result[0]
+    if status == 0:
+        print(f"Send `{message}` to topic `{topic}`")
+    else:
+        print(f"Failed to send message to topic {topic}")
+
+        
+
+def subscribe(client: mqtt_client, topic: str):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+            
         if (msg.topic=="auth/user"):
             message_json=json.loads(msg.payload.decode())
             user=message_json["user"]
@@ -16,3 +41,24 @@
             msg_a_envoyer={"param1":"param... A REMPLIR######################","status":"ok"}
             message_json=json.dumps(msg_a_envoyer)
             publish(client,f"auth/zone/{ID}", message_json)
+            
+        
+    client.subscribe(topic)
+    print(f"On est souscrit au topic {topic}")
+    client.on_message = on_message
+
+
+def run():
+    """Main function to run MQTT client"""
+
+    client = connect_mqtt()
+    
+    subscribe(client, "auth/zone")  
+    subscribe(client, "auth/user")  
+    
+    print(f"Edge Cluster ID: {EDGE_ID}")
+
+    client.loop_forever()
+
+if __name__ == '__main__':
+    run()
